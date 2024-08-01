6f825934445fe0174ba4c8028153ce7e6f4960de
Ahmad Mujahid <ahmadmujahid1950@gmail.com>
2024-08-01 17:01:39 +0500

    Intial Commit

---
+++ Brainwave/src/App.jsx
@@ -0,0 +1,18 @@
+import ButtonGradiant from "./assets/svg/ButtonGradient";
+// import Button from "./components/Button";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
+        <Header />
+        <Hero />
+      </div>
+      <ButtonGradiant />
+    </>
+  );
+};
+
+export default App;
